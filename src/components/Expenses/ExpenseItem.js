2a6166bf012f12c362caf1d343b7561dbc94e2f5
Abdelhady Salah <hadysalah1455@gmail.com>
2022-05-30 23:20:36 +0200

    Use li for semantic html

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,12 @@ import './ExpenseItem.css'
 
 export default function ExpenseItem(props) {
     return (
-        <div className={'expense-item'}>
+        <li className={'expense-item'}>
             <ExpenseDate date={props.date} />
             <div className={'expense-item__description'}>
                 <h2>{props.desc}</h2>
                 <div className={'expense-item__price'}>${props.price}</div>
             </div>
-        </div>
+        </li>
     )
 }
